987040d987a3b36a799031b6fee0c09d56d3ef01
anakinmorgan <anakinmorgan@gmail.com>
2020-08-16 20:07:51 +0100

    my first commit

--- 02-multiple-routes/MyNodeMultiple/app.js
+++ 02-multiple-routes/MyNodeMultiple/app.js
@@ -5,6 +5,14 @@ app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+app.get("/starwars", (req, res, next) => {
+  res.send("These are the list of Starwars Movies pre and post 2000 although i dont include the sequals because they are rubbish just type pre2000 or post2000 into the url");
+});
+
+app.get("/starwars/pre2000", (req, res, next) => {
+  res.json(["A New Hope", "The Empire Strikes Back", "Return Of The Jedi"]);
+});
+
+app.get("/starwars/post2000", (req, res, next) => {
+  res.json(["Phantom Menace", "Attack Of The Clones", "Revenge Of The Sith"]);
 });
